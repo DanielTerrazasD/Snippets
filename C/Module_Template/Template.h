1b957af2fa362437eb2fe21c4f31ba8c9ff1719f
DanTerra <daniel-terrazas@hotmail.com>
2020-05-19 12:13:05 -0600

    Refactoring multiple-line comments.

--- C/Module_Template/Template.h
+++ C/Module_Template/Template.h
@@ -1,37 +1,37 @@
 /*****************************************************************************/
-/* Copyright (c) 2020, Daniel Terrazas                                       */
-/* ------------------------------------------------------------------------- */
-/*                                Project Name                               */
-/* ------------------------------------------------------------------------- */
-/* FILE: Template.h                                                          */
-/* ------------------------------------------------------------------------- */
-/* DESCRIPTION:                                                              */
-/* Template.h description.                                                   */
-/* ------------------------------------------------------------------------- */
-/*                                                                           */
-/* Permission is hereby granted, free of charge, to any person obtaining a   */
-/* copy of this software and associated documentation files (the "Software") */
-/* to deal in the Software without restriction, including without limitation */
-/* the rights * to use, copy, modify, merge, publish, distribute,            */
-/* sublicense, and/or sell* copies of the Software, and to permit persons to */
-/* whom the Software is furnished to do so, subject to the following         */
-/* conditions:                                                               */
-/*                                                                           */
-/* The above copyright notice and this permission notice shall be included   */
-/* in all copies or substantial portions of the Software.                    */
-/*                                                                           */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   */
-/* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.    */
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY      */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT */
-/* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  */
-/* THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                */
-/*                                                                           */
+/** Copyright (c) 2020, Daniel Terrazas                                     **/
+/** ----------------------------------------------------------------------- **/
+/**                               Project Name                              **/
+/** ----------------------------------------------------------------------- **/
+/** FILE: Template.h                                                        **/
+/** ----------------------------------------------------------------------- **/
+/** DESCRIPTION:                                                            **/
+/** Template.h description.                                                 **/
+/** ----------------------------------------------------------------------- **/
+/**                                                                         **/
+/** Permission is hereby granted, free of charge, to any person obtaining a **/
+/** copy of this software and associated documentation files                **/
+/** (the "Software") to deal in the Software without restriction, including **/
+/** without limitation the rights * to use, copy, modify, merge, publish,   **/
+/** distribute, sublicense, and/or sell* copies of the Software, and to     **/
+/** permit persons to whom the Software is furnished to do so, subject to   **/
+/** the following conditions:                                               **/
+/**                                                                         **/
+/** The above copyright notice and this permission notice shall be included **/
+/** in all copies or substantial portions of the Software.                  **/
+/**                                                                         **/
+/** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS **/
+/** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              **/
+/** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  **/
+/** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY    **/
+/** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    **/
+/** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       **/
+/** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  **/
+/**                                                                         **/
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*                               Preprocessors                               */
+/**                               Preprocessors                             **/
 /*****************************************************************************/
 
 // Guard to prevent recursive inclusion
@@ -39,40 +39,40 @@
 #define FILE_TEMPLATE_H
 
 /*****************************************************************************/
-/*                                  Includes                                 */
+/**                                  Includes                               **/
 /*****************************************************************************/
 
 #include "Template_Config.h"
 
 /*****************************************************************************/
-/*                               Public Macros                               */
+/**                               Public Macros                             **/
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*                             Public Data Types                             */
+/**                             Public Data Types                           **/
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*                              Public Constants                             */
+/**                              Public Constants                           **/
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*                              Public Variables                             */
+/**                              Public Variables                           **/
 /*****************************************************************************/
 
 /*****************************************************************************/
-/*                              Public Functions                             */
+/**                              Public Functions                           **/
 /*****************************************************************************/
 
 
 #endif // FILE_TEMPLATE_H
 
 /*****************************************************************************/
-/* Revision Log:                                                             */
-/* ------------------------------------------------------------------------- */
-/* Created By:         Revision      Date:          Description:             */
-/* ------------------------------------------------------------------------- */
-/* Daniel Terrazas     0.0.0.1     3-Mar-2020      Initial Revision.         */
-/* ------------------------------------------------------------------------- */
-/*                                END OF FILE                                */
+/** Revision Log:                                                           **/
+/** ----------------------------------------------------------------------- **/
+/** Created By:         Revision      Date:          Description:           **/
+/** ----------------------------------------------------------------------- **/
+/** Daniel Terrazas     0.0.0.1     3-Mar-2020      Initial Revision.       **/
+/** ----------------------------------------------------------------------- **/
+/**                                END OF FILE                              **/
 /*****************************************************************************/
