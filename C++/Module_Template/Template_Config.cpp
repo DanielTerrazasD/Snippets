b46fdaa1973faecff31bf72c08ab116ed6abafc6
DanielTerrazasD <62128296+DanielTerrazasD@users.noreply.github.com>
2020-11-22 21:42:51 -0700

    Fixed some typos in copyright noticed and removed revision log from files.

--- C++/Module_Template/Template_Config.cpp
+++ C++/Module_Template/Template_Config.cpp
@@ -12,8 +12,8 @@
 /** Permission is hereby granted, free of charge, to any person obtaining a **/
 /** copy of this software and associated documentation files                **/
 /** (the "Software") to deal in the Software without restriction, including **/
-/** without limitation the rights * to use, copy, modify, merge, publish,   **/
-/** distribute, sublicense, and/or sell* copies of the Software, and to     **/
+/** without limitation the rights to use, copy, modify, merge, publish,     **/
+/** distribute, sublicense, and/or sell copies of the Software, and to      **/
 /** permit persons to whom the Software is furnished to do so, subject to   **/
 /** the following conditions:                                               **/
 /**                                                                         **/
@@ -69,11 +69,5 @@
 /*****************************************************************************/
 
 /*****************************************************************************/
-/** Revision Log:                                                           **/
-/** ----------------------------------------------------------------------- **/
-/** Created By:         Revision      Date:          Description:           **/
-/** ----------------------------------------------------------------------- **/
-/** Daniel Terrazas     0.0.0.1     3-Mar-2020      Initial Revision.       **/
-/** ----------------------------------------------------------------------- **/
 /**                                END OF FILE                              **/
 /*****************************************************************************/
